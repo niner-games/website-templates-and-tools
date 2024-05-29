7f523c8b28c539c1a77f07843f455a23e4b6abf9
trejder <2903392+trejder@users.noreply.github.com>
2024-05-29 14:47:35 +0200

    Add media queries overlay

--- domains/test.lostmine.cc/public_html/prototypes/base-prototype/translation.js
+++ domains/test.lostmine.cc/public_html/prototypes/base-prototype/translation.js
@@ -1,50 +1,127 @@
 let translation = {
     pl:
     {
-        "First code the default layout (992 px),": "",
-        "and then use CSS3 media queries": "",
-        "to code several child layouts,": "",
-        "i.e.: 768 px, 480 px, and 320 px.": "",
+        "Extra large screens, UHDs (2000 px width).": "Bardzo duże ekrany, UHD (szerokość 2000 px).",
+        "Large screens, TVs, wide screens (1200 px width).": "Duże ekrany, telewizory, szerokie ekrany (szerokość 1200 px).",
+        "Medium screens, PCs, desktops (992 px width).": "ekrany średnie, komputery stacjonarne (szerokość 992 px).",
+        "Small screens, tablets (768 px width).": "Małe ekrany, tablety (szerokość 768 px).",
+        "Extra small screens, phones (480 px width).": "Bardzo małe ekrany, telefony (szerokość 480 px).",
+        "First code the default layout (2000 px),": "Najpierw zakoduj domyślny układ (2000 px),",
+        "and then use CSS3 media queries": "a następnie użyj zapytań o media CSS3",
+        "to code several child layouts,": "aby zakodować kilka układów podrzędnych,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "tj.: 1200 px, 992 px, 768 px, 480 px i 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "licencja ogólna Creative Commons Uznanie autorstwa. Na tych samych warunkach 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Patrząc na północny wschód poprzez rzekę San Juan na wyspie Vancouver w Kolumbii Brytyjskiej."
     },
     sx:
     {
+        "Extra large screens, UHDs (2000 px width).": "Barzo sroge ekrany, UHD (szyrokość 2000 px).",
+        "Large screens, TVs, wide screens (1200 px width).": "Sroge ekrany, telewizory, szyroke ekrany (szyrokość 1200 px).",
+        "Medium screens, PCs, desktops (992 px width).": "ekrany postrzednie, kōmputry stacjonarne (szyrokość 992 px).",
+        "Small screens, tablets (768 px width).": "Mołe ekrany, tablyty (szyrokość 768 px).",
+        "Extra small screens, phones (480 px width).": "Barzo mołe ekrany, telefōny (szyrokość 480 px).",
+        "First code the default layout (2000 px),": "Nojprzōd zakoduj wychodny ukłŏd (2000 px),",
+        "and then use CSS3 media queries": "a potym użyj spytań ô media CSS3",
+        "to code several child layouts,": "coby zakodować pŏrã układōw podrzyndnych,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "tj.: 1200 px, 992 px, 768 px, 480 px i 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Ôgōlnŏ licyncyjŏ Creative Commons Uznanie autorstwa – Na tych samych warōnkach 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Patrzynie na pōłnocny wschōd bez rzykã San Juan na wyspie Vancouver w Kolumbije Brytyjskij."
     },
     de:
     {
+        "Extra large screens, UHDs (2000 px width).": "Extragroße Bildschirme, UHDs (2000 px Breite).",
+        "Large screens, TVs, wide screens (1200 px width).": "Große Bildschirme, Fernseher, Breitbildschirme (1200 px Breite).",
+        "Medium screens, PCs, desktops (992 px width).": "Mittlere Bildschirme, PCs, Desktops (992 px Breite).",
+        "Small screens, tablets (768 px width).": "Kleine Bildschirme, Tablets (768 px Breite).",
+        "Extra small screens, phones (480 px width).": "Besonders kleine Bildschirme, Telefone (480 px Breite).",
+        "First code the default layout (2000 px),": "Coden Sie zunächst das Standardlayout (2000 px),",
+        "and then use CSS3 media queries": "und verwenden Sie dann CSS3-Medienabfragen",
+        "to code several child layouts,": "mehrere untergeordnete Layouts zu codieren,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "z. B.: 1200 px, 992 px, 768 px, 480 px und 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Creative Commons Attribution-Share Alike 2.0 Generische Lizenz",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Blick nach Nordosten über den San Juan River auf Vancouver Island, British Columbia."
     },
     es:
     {
+        "Extra large screens, UHDs (2000 px width).": "Pantallas extra grandes, UHD (2000 px de ancho).",
+        "Large screens, TVs, wide screens (1200 px width).": "Pantallas grandes, televisores, pantallas anchas (1200 px de ancho).",
+        "Medium screens, PCs, desktops (992 px width).": "Pantallas medianos, PC, escritorios (992 px de ancho).",
+        "Small screens, tablets (768 px width).": "Pantallas pequeños, tabletas (768 px de ancho).",
+        "Extra small screens, phones (480 px width).": "Pantallas extra pequeñas, teléfonos (480 px de ancho).",
+        "First code the default layout (2000 px),": "Primero codifica el diseño predeterminado (2000 px),",
+        "and then use CSS3 media queries": "y luego usar consultas de medios CSS3",
+        "to code several child layouts,": "para codificar varios diseños secundarios,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "es decir: 1200 px, 992 px, 768 px, 480 px y 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licencia genérica Creative Commons Atribución-Compartir Igual 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Mirando al noreste a través del río San Juan en la isla de Vancouver, Columbia Británica."
     },
     fr:
     {
+        "Extra large screens, UHDs (2000 px width).": "Écrans très grands, UHD (largeur 2000 px).",
+        "Large screens, TVs, wide screens (1200 px width).": "Grands écrans, téléviseurs, écrans larges (largeur 1200 px).",
+        "Medium screens, PCs, desktops (992 px width).": "Écrans moyens, PC, ordinateurs de bureau (largeur 992 px).",
+        "Small screens, tablets (768 px width).": "Petits écrans, tablettes (largeur 768 px).",
+        "Extra small screens, phones (480 px width).": "Écrans très petits, téléphones (largeur 480 px).",
+        "First code the default layout (2000 px),": "Codez d'abord la mise en page par défaut (2000 px),",
+        "and then use CSS3 media queries": "puis utilisez les requêtes multimédias CSS3",
+        "to code several child layouts,": "coder plusieurs mises en page enfants,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "c'est-à-dire : 1200 px, 992 px, 768 px, 480 px et 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licence générique Creative Commons Attribution-Partage dans les mêmes conditions 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Vue vers le nord-est, de l'autre côté de la rivière San Juan, sur l'île de Vancouver, en Colombie-Britannique."
     },
     it:
     {
+        "Extra large screens, UHDs (2000 px width).": "Schermi extra large, UHD (larghezza 2000 px).",
+        "Large screens, TVs, wide screens (1200 px width).": "Schermi di grandi dimensioni, TV, schermi larghi (larghezza 1200 px).",
+        "Medium screens, PCs, desktops (992 px width).": "Schermi medi, PC, desktop (larghezza 992 px).",
+        "Small screens, tablets (768 px width).": "Piccoli schermi, tablet (larghezza 768 px).",
+        "Extra small screens, phones (480 px width).": "Schermi extra piccoli, telefoni (larghezza 480 px).",
+        "First code the default layout (2000 px),": "Per prima cosa codifica il layout predefinito (2000 px),",
+        "and then use CSS3 media queries": "e quindi utilizzare le query multimediali CSS3",
+        "to code several child layouts,": "per codificare diversi layout figli,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "ovvero: 1200 px, 992 px, 768 px, 480 px e 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licenza generica Creative Commons Attribuzione-Condividi allo stesso modo 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Guardando a nord-est attraverso il fiume San Juan sull'isola di Vancouver, nella Columbia Britannica."
     },
     hi:
     {
+        "Extra large screens, UHDs (2000 px width).": "अतिरिक्त बड़े उपकरण, UHDs (2000 px चौड़ाई).",
+        "Large screens, TVs, wide screens (1200 px width).": "बड़े उपकरण, टीवी, चौड़ी स्क्रीन (1200 px चौड़ाई).",
+        "Medium screens, PCs, desktops (992 px width).": "मध्यम स्क्रीन, पीसी, डेस्कटॉप (992 px चौड़ाई).",
+        "Small screens, tablets (768 px width).": "छोटे उपकरण, टैबलेट (768 px चौड़ाई).",
+        "Extra small screens, phones (480 px width).": "अतिरिक्त छोटी स्क्रीन, फोन (480 px चौड़ाई).",
+        "First code the default layout (2000 px),": "सबसे पहले डिफ़ॉल्ट लेआउट (2000 px) को कोड करें,",
+        "and then use CSS3 media queries": "और फिर CSS3 मीडिया क्वेरीज़ का उपयोग करें",
+        "to code several child layouts,": "कई चाइल्ड लेआउट को कोड करने के लिए,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "अर्थात: 1200 px, 992 px, 768 px, 480 px, और 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "क्रिएटिव कॉमन्स एट्रिब्यूशन-शेयर अलाइक 2.0 जेनेरिक लाइसेंस",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "ब्रिटिश कोलंबिया के वैंकूवर द्वीप पर सैन जुआन नदी के पार उत्तर-पूर्व की ओर देखते हुए।"
     },
     uk:
     {
+        "Extra large screens, UHDs (2000 px width).": "Дуже великі екрани, UHD (ширина 2000 px).",
+        "Large screens, TVs, wide screens (1200 px width).": "Великі екрани, телевізори, широкі екрани (ширина 1200 px).",
+        "Medium screens, PCs, desktops (992 px width).": "Середні екрани, ПК, настільні комп’ютери (ширина 992 px).",
+        "Small screens, tablets (768 px width).": "Невеликі екрани, планшети (ширина 768 px).",
+        "Extra small screens, phones (480 px width).": "Дуже маленькі екрани, телефони (ширина 480 px).",
+        "First code the default layout (2000 px),": "Спочатку кодуйте макет за замовчуванням (2000 px),",
+        "and then use CSS3 media queries": "а потім використовувати медіа-запити CSS3",
+        "to code several child layouts,": "кодувати кілька дочірніх макетів,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "тобто: 1200 px, 992 px, 768 px, 480 px і 320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Загальна ліцензія Creative Commons Attribution-Share Alike 2.0",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Дивлячись на північний схід через річку Сан-Хуан на острові Ванкувер, Британська Колумбія."
     },
     iw:
     {
+        "Extra large screens, UHDs (2000 px width).": "מכשירpxם במיוחד, UHDs (pxל 2000 פיקסלים).",
+        "Large screens, TVs, wide screens (1200 px width).": "מכשירpxם, טלוויזיות, מסכים רחבים (1200 פיקסלים רוחב).",
+        "Medium screens, PCs, desktops (992 px width).": "מסכים בינוניים, מחשבים אישיים, שולחניים (992 פיקסלים רוחב).",
+        "Small screens, tablets (768 px width).": "מסכים קטנים, טאבלטים (768 פיקסלים רוחב).",
+        "Extra small screens, phones (480 px width).": "מסכי px מיוחד, px (480 פיקסלים רוחב).",
+        "First code the default layout (2000 px),": "תחילה קוד את פריסת ברירת המחדל (2000 px),",
+        "and then use CSS3 media queries": "ולאחר מכן השתמש בשאילתות מדיה ב-CSS3",
+        "to code several child layouts,": "לקוד מספר פריסות ילדים,",
+        "i.e.: 1200 px, 992 px, 768 px, 480 px, and 320 px.": "כלומר: 1200 px, 992 px, 768 px, 480 px ו-320 px.",
         "Creative Commons Attribution-Share Alike 2.0 Generic License": "Creative Commons ייחוס-שיתוף זהה 2.0 רישיון כללי",
         "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "מבט לצפון-מזרח מעבר לנהר סן חואן באי ונקובר, קולומביה הבריטית."
     }
