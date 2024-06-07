feaf1e2ebd81935d97769fc4e57634d7d214b064
trejder <2903392+trejder@users.noreply.github.com>
2024-06-08 00:38:50 +0200

    First version of "Main Viewport" prototype

---
+++ domains/test.lostmine.cc/public_html/prototypes/main-viewport/engine/translation-engine.js
@@ -0,0 +1,119 @@
+const defaultLanguageCode = 'en';
+const defaultLanguageOverlayFontSize = '14px';
+const defaultLanguageOverlayElementId = 'language-overlay';
+
+window.addEventListener("load", function() {
+    LanguageBox.createOverlay();
+    LanguageBox.translateDocument(LanguageBox.getLanguage());
+});
+
+const LanguageBox = {
+    id: defaultLanguageOverlayElementId,
+    languages: {
+        "en": "English",
+        "pl": "polski",
+        "de": "Deutsch",
+        "es": "Español",
+        "fr": "Français",
+        "it": "Italiano",
+        "sx": "ślōnskŏ gŏdka",
+        "hi": "हिन्दी",
+        "uk": "Українська",
+        "iw": "שפה עברית"
+    },
+    
+    isSet: function (variable, def = false) {
+        if (typeof variable === 'string') {
+            return (variable.trim() !== '') ? variable : def;
+        } else if (typeof variable === 'object') {
+            return variable
+        } else return def;
+    },
+    
+    getId() {
+        return this.isSet(this.id, defaultLanguageOverlayElementId);
+    },
+    
+    getLanguage() {
+        return window.localStorage.getItem("language") || defaultLanguageCode;
+    },
+
+    getOverlay: function() {
+        let overlay = document.getElementById(this.getId());
+        
+        return this.isSet(overlay) ? overlay : this.createOverlay();
+    },
+    
+    createOverlay: function() {
+        let style = "direction: ltr;";
+        let overlay = document.createElement("div");
+        let select = document.createElement("select");
+        
+        overlay.id = this.getId();
+        
+        overlay.style.left = "0";
+        overlay.style.bottom = "0";
+        overlay.style.padding = "3px";
+        overlay.style.position = "fixed";
+        overlay.style.margin = "0 0 7px 7px";
+        overlay.style.fontSize = defaultLanguageOverlayFontSize;
+        overlay.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+        overlay.style.border = "solid 2px rgba(0, 0, 0, 0.3)";
+        
+        for (var key in this.languages) {
+            if (this.languages.hasOwnProperty(key)) {
+                let option = document.createElement("option");
+                
+                style = (key === "iw") ? "direction: rtl;" : style;
+                
+                option.value = key;
+                option.style = style;
+                option.text = this.languages[key];
+                
+                select.appendChild(option);
+            }
+        }
+        
+        select.id = "the-select";
+        select.value = this.getLanguage();
+        
+        select.style.fontSize = defaultLanguageOverlayFontSize;
+        
+        select.addEventListener("change", this.handleChange);
+        
+        overlay.appendChild(select);
+        document.body.appendChild(overlay);
+    },
+    
+    handleChange: function() {
+        selected = this.options[this.selectedIndex].value;
+
+        LanguageBox.translateDocument(selected);        
+        window.localStorage.setItem("language", selected);
+    },
+    
+    translateDocument: function(language) {
+        let tokenRegex = /setText\('[^']*', '([^']+)'\)/;
+        let matchingNodes = document.querySelectorAll('[text]');
+        let style = (language === "iw") ? "direction: rtl;" : "direction: ltr;";
+        
+        if (matchingNodes.length === 0) return null;
+        if (typeof language === 'undefined') return null;
+        if (typeof translation === 'undefined') return null;
+        
+        matchingNodes.forEach(function(matchingElement) {
+            let match = matchingElement.getAttribute("text").match(tokenRegex);
+            
+            if (match) {
+                let sourceText = match[1];
+                let translatedText = sourceText;
+                
+                translatedText = (typeof translation[language] === "undefined") ? sourceText : translation[language][sourceText];
+                translatedText = (typeof translatedText === "undefined" || translatedText === "") ? sourceText : translatedText;
+                
+                matchingElement.style = style;                
+                matchingElement.textContent = translatedText;
+            }
+        });
+    }
+};
---
+++ domains/test.lostmine.cc/public_html/prototypes/main-viewport/translation.js
@@ -0,0 +1,137 @@
+let translation = {
+    pl:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (ekrany Ultra HD, banery elektroniczne)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px do 1800 px (ekrany Full HD, duże monitory)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px do 1200 px (ekrany HD, komputery stacjonarne)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px do 1024 px (małe ekrany, laptopy)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: 481 px do 768 px (tablety)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px do 480 px (telefony)",
+        "First code the default layout (2000 px),": "Najpierw zakoduj domyślny układ (2000 px),",
+        "and then use CSS3 media queries": "a następnie użyj zapytań o media CSS3",
+        "to code several child layouts,": "aby zakodować kilka układów podrzędnych,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "to znaczy: 1800 px, 1200 px, 1024 px, 768 px i 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "licencja ogólna Creative Commons Uznanie autorstwa. Na tych samych warunkach 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Patrząc na północny wschód poprzez rzekę San Juan na wyspie Vancouver w Kolumbii Brytyjskiej."
+    },
+    sx:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (ekrany Ultra HD, banery elektrōniczne)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px do 1800 px (ekrany Full HD, sroge mōnitory)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px do 1200 px (ekrany HD, kōmputry stacjonarne)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px do 1024 px (małe ekrany, laptopy)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: 481 px do 768 px (tablety)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px do 480 px (telefōny)",
+        "First code the default layout (2000 px),": "Nojprzōd zakoduj wychodny ukłŏd (2000 px),",
+        "and then use CSS3 media queries": "a potym użyj spytań ô media CSS3",
+        "to code several child layouts,": "coby zakodować pŏrã układōw podrzyndnych,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "to znoczy: 1800 px, 1200 px, 1024 px, 768 px i 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Ôgōlnŏ licyncyjŏ Creative Commons Uznanie autorstwa – Na tych samych warōnkach 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Patrzynie na pōłnocny wschōd bez rzykã San Juan na wyspie Vancouver w Kolumbije Brytyjskij."
+    },
+    de:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ Pixel (Ultra-HD-Bildschirme, Banneranzeigen)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px bis 1800 px (Full HD-Bildschirme, große Displays)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px bis 1200 px (HD-Bildschirme, Desktops)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px bis 1024 px (kleine Bildschirme, Laptops)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: 481 px bis 768 px (Tablets)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px bis 480 px (Telefone)",
+        "First code the default layout (2000 px),": "Coden Sie zunächst das Standardlayout (2000 px),",
+        "and then use CSS3 media queries": "und verwenden Sie dann CSS3-Medienabfragen",
+        "to code several child layouts,": "mehrere untergeordnete Layouts zu codieren,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "z. B.: 1800 px, 1200 px, 1024 px, 768 px und 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Creative Commons Attribution-Share Alike 2.0 Generische Lizenz",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Blick nach Nordosten über den San Juan River auf Vancouver Island, British Columbia."
+    },
+    es:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (pantallas Ultra HD, visualizaciones de pancartas)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px a 1800 px (pantallas Full HD, pantallas grandes)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px a 1200 px (pantallas HD, escritorios)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px a 1024 px (pantallas pequeñas, portátiles)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: de 481 px a 768 px (tabletas)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px a 480 px (teléfonos).",
+        "First code the default layout (2000 px),": "Primero codifica el diseño predeterminado (2000 px),",
+        "and then use CSS3 media queries": "y luego usar consultas de medios CSS3",
+        "to code several child layouts,": "para codificar varios diseños secundarios,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "es decir: 1800 px, 1200 px, 1024 px, 768 px y 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licencia genérica Creative Commons Atribución-Compartir Igual 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Mirando al noreste a través del río San Juan en la isla de Vancouver, Columbia Británica."
+    },
+    fr:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6 : 1800+ px (écrans Ultra HD, affichages de bannières)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px à 1800 px (écrans Full HD, grands écrans)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4 : 1025 px à 1200 px (écrans HD, ordinateurs de bureau)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3 : 769 px à 1024 px (petits écrans, ordinateurs portables)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2 : 481 px à 768 px (tablettes)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1 : 320 px à 480 px (téléphones)",
+        "First code the default layout (2000 px),": "Codez d'abord la mise en page par défaut (2000 px),",
+        "and then use CSS3 media queries": "puis utilisez les requêtes multimédias CSS3",
+        "to code several child layouts,": "coder plusieurs mises en page enfants,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "soit : 1800 px, 1200 px, 1024 px, 768 px et 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licence générique Creative Commons Attribution-Partage dans les mêmes conditions 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Vue vers le nord-est, de l'autre côté de la rivière San Juan, sur l'île de Vancouver, en Colombie-Britannique."
+    },
+    it:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (schermi Ultra HD, display banner)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: da 1201 px a 1800 px (schermi Full HD, display di grandi dimensioni)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: da 1025 px a 1200 px (schermi HD, desktop)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: da 769 px a 1024 px (piccoli schermi, laptop)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: da 481 px a 768 px (tablet)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: da 320 px a 480 px (telefoni)",
+        "First code the default layout (2000 px),": "Per prima cosa codifica il layout predefinito (2000 px),",
+        "and then use CSS3 media queries": "e quindi utilizzare le query multimediali CSS3",
+        "to code several child layouts,": "per codificare diversi layout figli,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "ovvero: 1800 px, 1200 px, 1024 px, 768 px e 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Licenza generica Creative Commons Attribuzione-Condividi allo stesso modo 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Guardando a nord-est attraverso il fiume San Juan sull'isola di Vancouver, nella Columbia Britannica."
+    },
+    hi:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (अल्ट्रा HD स्क्रीन, बैनर डिस्प्ले)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px से 1800 px (पूर्ण HD स्क्रीन, बड़े डिस्प्ले)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px से 1200 px (HD स्क्रीन, डेस्कटॉप)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px से 1024 px (छोटी स्क्रीन, लैपटॉप)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: 481 px से 768 px (टैबलेट)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px से 480 px (फ़ोन)",
+        "First code the default layout (2000 px),": "सबसे पहले डिफ़ॉल्ट लेआउट (2000 px) को कोड करें,",
+        "and then use CSS3 media queries": "और फिर CSS3 मीडिया क्वेरीज़ का उपयोग करें",
+        "to code several child layouts,": "कई चाइल्ड लेआउट को कोड करने के लिए,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "अर्थात: 1800 पिक्सेल, 1200 पिक्सेल, 1024 पिक्सेल, 768 पिक्सेल और 480 पिक्सेल।",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "क्रिएटिव कॉमन्स एट्रिब्यूशन-शेयर अलाइक 2.0 जेनेरिक लाइसेंस",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "ब्रिटिश कोलंबिया के वैंकूवर द्वीप पर सैन जुआन नदी के पार उत्तर-पूर्व की ओर देखते हुए।"
+    },
+    uk:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (екрани Ultra HD, банерні дисплеї)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: від 1201 до 1800 px (екрани Full HD, великі дисплеї)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: від 1025 до 1200 px (екрани HD, настільні комп’ютери)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: від 769 px до 1024 px (малі екрани, ноутбуки)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: від 481 до 768 px (планшети)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: від 320 до 480 px (телефони)",
+        "First code the default layout (2000 px),": "Спочатку кодуйте макет за замовчуванням (2000 px),",
+        "and then use CSS3 media queries": "а потім використовувати медіа-запити CSS3",
+        "to code several child layouts,": "кодувати кілька дочірніх макетів,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "тобто: 1800 px, 1200 px, 1024 px, 768 px і 480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Загальна ліцензія Creative Commons Attribution-Share Alike 2.0",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "Дивлячись на північний схід через річку Сан-Хуан на острові Ванкувер, Британська Колумбія."
+    },
+    iw:
+    {
+        "MQ6: 1800+ px (Ultra HD screens, banner displays)": "MQ6: 1800+ px (מסכי Ultra HD, תצוגות באנר)",
+        "MQ5: 1201 px to 1800 px (Full HD screens, big displays)": "MQ5: 1201 px עד 1800 px (מסכי HD מלא, צגים גדולים)",
+        "MQ4: 1025 px to 1200 px (HD screens, desktops)": "MQ4: 1025 px עד 1200 px (מסכי HD, שולחנות עבודה)",
+        "MQ3: 769 px to 1024 px (small screens, laptops)": "MQ3: 769 px עד 1024 px (מסכים קטנים, מחשבים ניידים)",
+        "MQ2: 481 px to 768 px (tablets)": "MQ2: 481 px עד 768 px (טאבלטים)",
+        "MQ1: 320 px to 480 px (phones)": "MQ1: 320 px עד 480 px (טלפונים)",
+        "First code the default layout (2000 px),": "תחילה קוד את פריסת ברירת המחדל (2000 px),",
+        "and then use CSS3 media queries": "ולאחר מכן השתמש בשאילתות מדיה ב-CSS3",
+        "to code several child layouts,": "לקוד מספר פריסות ילדים,",
+        "i.e.: 1800 px, 1200 px, 1024 px, 768 px and 480 px.": "כלומר: 1800 px, 1200 px, 1024 px, 768 px ו-480 px.",
+        "Creative Commons Attribution-Share Alike 2.0 Generic License": "Creative Commons ייחוס-שיתוף זהה 2.0 רישיון כללי",
+        "Looking northeast across the San Juan River on Vancouver Island, British Columbia.": "מבט לצפון-מזרח מעבר לנהר סן חואן באי ונקובר, קולומביה הבריטית."
+    }
+};
